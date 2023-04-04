ac99ff184e9f83232f72a7508000d8053abbe685
Abissa <a.sani@alustudent.com>
2023-04-04 08:57:56 +0200

    fix cities

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -8,6 +8,32 @@ from models.city import City
 from models.state import State
 
 
+@app_views.route('/cities/<city_id>', methods=['GET', 'DELETE', 'PUT'])
+def cities_by_id(city_id=None):
+    """City by id"""
+    city = storage.get(City, city_id)
+    if city is None:
+        abort(404)
+
+    if request.method == 'GET':
+        return jsonify(city.to_dict())
+
+    if request.method == 'DELETE':
+        city.delete()
+        del city
+        return jsonify({}), 200
+
+    if request.method == 'PUT':
+        data = request.get_json()
+        if data is None:
+            abort(400, 'Not a JSON')
+        for key, val in data.items():
+            if key not in ['id', 'state_id', 'created_at', 'updated_at']:
+                setattr(city, key, val)
+        city.save()
+        return jsonify(city.to_dict()), 200
+
+
 @app_views.route('/states/<state_id>/cities', methods=['GET', 'POST'])
 def cities_by_state(state_id=None):
     """Return cities by state"""
@@ -33,29 +59,3 @@ def cities_by_state(state_id=None):
         new_city = City(**data)
         new_city.save()
         return jsonify(new_city.to_dict()), 201
-
-
-@app_views.route('/cities/<city_id>', methods=['GET', 'DELETE', 'PUT'])
-def cities_by_id(city_id=None):
-    """City by id"""
-    city = storage.get(City, city_id)
-    if city is None:
-        abort(404)
-
-    if request.method == 'GET':
-        return jsonify(city.to_dict())
-
-    if request.method == 'DELETE':
-        city.delete()
-        del city
-        return jsonify({}), 200
-
-    if request.method == 'PUT':
-        data = request.get_json()
-        if data is None:
-            abort(400, 'Not a JSON')
-        for key, val in data.items():
-            if key not in ['id', 'state_id', 'created_at', 'updated_at']:
-                setattr(city, key, val)
-        city.save()
-        return jsonify(city.to_dict()), 200
