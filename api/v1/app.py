06906461f6912e7cf5e90e25ae9fec7076f910c6
Abissa <a.sani@alustudent.com>
2023-04-04 06:06:27 +0200

    minor changes task 4

--- api/v1/app.py
+++ api/v1/app.py
@@ -2,19 +2,21 @@
 """first api with flask and python"""
 
 from flask import Flask, jsonify
-from flask_cors import CORS
+# from flask_cors import CORS
 from models import storage
 from api.v1.views import app_views
 from os import getenv
 
-app = Flask('v1')
+app = Flask(__name__)
 
 app.register_blueprint(app_views)
-CORS(app, resources=r"/api/v1/*", origins="*")
-app.url_map.strict_slashes = False  # allow /api/v1/states/ and /api/v1/states
-host = getenv('HBNB_API_HOST')
-port = getenv('HBNB_API_PORT')
-threaded = True if getenv('HBNB_API_HOST') else False
+
+
+# CORS(app, resources=r"/api/v1/*", origins="*")
+# app.url_map.strict_slashes = False  # allow /api/v1/states/ and /api/v1/states
+
+
+# threaded = True if getenv('HBNB_API_HOST') else False
 
 
 @app.errorhandler(404)
@@ -30,9 +32,8 @@ def teardown(*args, **kwargs):
 
 
 if __name__ == "__main__":
-    if host is None:
-        HBNB_API_HOST = '0.0.0.0'
-    if port is None:
-        HBNB_API_PORT = 5000
+    host = getenv("HBNB_API_HOST", "0.0.0.0")
+    port = getenv("HBNB_API_PORT", "5000")
+
     print(app.url_map)
-    app.run(host="0.0.0.0", port=5000, debug=True, threaded=threaded)
+    app.run(host=host, port=port, debug=True)
